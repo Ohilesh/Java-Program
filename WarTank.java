549293436e9b96837e787a311bc08163c3d6f58e
unknown <ohilesh.xworkz@gmail.com>
2024-08-22 19:41:17 +0530

    Hello Ohilesh

---
+++ WarTank.java
@@ -0,0 +1,21 @@
+class WarTank {
+	
+    public static void time(double reloadTime) 
+	{
+		reloadTime= 1.5;
+        System.out.println("The Reload Time after Each Fire == " +reloadTime + "minutes");
+    }
+	
+	 public static void capacity(int fuelCapacity) 
+	{
+		fuelCapacity= 100;
+        System.out.println("The Fuel Capacity of Tank == " +fuelCapacity + "Liters");
+    }
+}
+	
+	
+
+
+
+
+
---
+++ WarTankRunner.java
@@ -0,0 +1,19 @@
+class WarTankRunner
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Start of main method"); 
+		WarTank.time(1.5);
+		WarTank.capacity(100);
+		
+	    System.out.println("End of main method");
+			
+	}
+}
+
+
+
+
+
+
+
